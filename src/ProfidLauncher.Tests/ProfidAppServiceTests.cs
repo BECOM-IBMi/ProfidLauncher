c5178cebd3df67eff88cf034fec8cf052ea04bfa
Michael Prattinger <michael.prattinger@becom-group.com>
2025-09-17 09:53:56 +0200

    fixing the testcases to match the new configuration settings

--- src/ProfidLauncher.Tests/ProfidAppServiceTests.cs
+++ src/ProfidLauncher.Tests/ProfidAppServiceTests.cs
@@ -18,7 +18,10 @@ public class ProfidAppServiceTests
         _settings = new ProfidLauncherSettings
         {
             HttpPort = 8082,
-            HttpsPort = 8085
+            HttpsPort = 8085,
+            Prod = "as400",
+            Test = "as400test",
+            China = "032-itsv-as400.becom.at"
         };
 
         setupAppModes();
@@ -34,7 +37,7 @@ public class ProfidAppServiceTests
     [InlineData("ATRIUMCN", 0, false, "032-GRPC-STAFF1", "http://032-itsv-as400.becom.at:8082/profoundui/atrium?workstnid=staff1&suffixid=1")]
     [InlineData("ATRIUMP", 8085, true, "001-itsv-mprat2", "https://as400:8085/profoundui/auth/atrium?workstnid=mprat2&suffixid=1")]
     [InlineData("ATRIUMP", 1234, false, "001-itsv-mprat2", "http://as400:1234/profoundui/atrium?workstnid=mprat2&suffixid=1")]
-    [InlineData("ATRIUMP", 0, false, "032-ADNB-RES1", "http://032-itsv-as400.becom.at:8082/profoundui/atrium?workstnid=mprat2&suffixid=1")]
+    [InlineData("ATRIUMCN", 0, false, "032-ADNB-RES1", "http://032-itsv-as400.becom.at:8082/profoundui/atrium?workstnid=res1&suffixid=1")]
     public void Test_GetUrl_OK(string operationMode, int port, bool useSecure, string hostName, string resultUrl)
     {
         var config = ProfidAppExtensions.GetProfidAppConfiguration(new CommandLineOptions { OperationMode = operationMode, Port = port, UseSecure = useSecure }, hostName);
@@ -52,48 +55,54 @@ public class ProfidAppServiceTests
         {
             OperationMode = "ATRIUMP",
             IconName = "favicon.ico",
-            BaseUrl = "{0}://as400.becom.at:{1}/profoundui/{2}atrium?workstnid={3}&suffixid=1",
+            BaseUrl = "{0}://{1}:{2}/profoundui/{3}atrium?workstnid={4}&suffixid=1",
             ProgramShortcutName = "Profid Launcher",
+            System = Models.System.PROD,
             UseHttps = false
         },
         new AppMode
         {
             OperationMode = "GENIEP",
             IconName = "favicon_genie.ico",
-            BaseUrl = "{0}://as400.becom.at:{1}/profoundui/{2}genie?skin=BECOM&workstnid={3}&suffixid=1",
+            BaseUrl = "{0}://{1}:{2}/profoundui/{3}genie?skin=BECOM&workstnid={4}&suffixid=1",
             ProgramShortcutName = "Genie Production",
+            System = Models.System.PROD,
             UseHttps = false
         },
         new AppMode
         {
             OperationMode = "ATRIUMD",
             IconName = "favicon_dev.ico",
-            BaseUrl = "{0}://as400test.becom.at:{1}/profoundui/{2}atrium?workstnid={3}&suffixid=1",
+            BaseUrl = "{0}://{1}:{2}/profoundui/{3}atrium?workstnid={4}&suffixid=1",
             ProgramShortcutName = "Profid Development",
+            System = Models.System.DEV,
             UseHttps = false
         },
         new AppMode
         {
             OperationMode = "GENIED",
             IconName = "favicon_genie_dev.ico",
-            BaseUrl = "{0}://as400test.becom.at:{1}/profoundui/{2}genie?skin=BECOM&workstnid={3}&suffixid=1",
+            BaseUrl = "{0}://{1}:{2}/profoundui/{3}genie?skin=BECOM&workstnid={4}&suffixid=1",
             ProgramShortcutName = "Genie Development",
+            System = Models.System.DEV,
             UseHttps = false
         },
         new AppMode
         {
             OperationMode = "ATRIUMCN",
             IconName = "favicon_cn.ico",
-            BaseUrl = "{0}://032-itsv-as400.becom.at:{1}/profoundui/{2}atrium?workstnid={3}&suffixid=1",
+            BaseUrl = "{0}://{1}:{2}/profoundui/{3}atrium?workstnid={4}&suffixid=1",
             ProgramShortcutName = "Profid Development",
+            System = Models.System.CHINA,
             UseHttps = false
         },
         new AppMode
         {
             OperationMode = "GENIECN",
             IconName = "favicon_genie_cn.ico",
-            BaseUrl = "{0}://032-itsv-as400.becom.at:{1}/profoundui/{2}genie?skin=BECOM&workstnid={3}&suffixid=1",
+            BaseUrl = "{0}://{1}:{2}/profoundui/{3}genie?skin=BECOM&workstnid={4}&suffixid=1",
             ProgramShortcutName = "Genie Development",
+            System = Models.System.CHINA,
             UseHttps = false
         }};
     }
